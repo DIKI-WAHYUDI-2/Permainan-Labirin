c497c1031f2d6d45b5ed6f3c868d9bfa98540c78
Diki Wahyudi <muhammaddikiw02@gmail.com>
2023-12-15 09:37:20 +0700

    5% lagi

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.mazegame {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+//    requires javafx.web;
 
 //    requires org.controlsfx.controls;
 //    requires com.dlsc.formsfx;
